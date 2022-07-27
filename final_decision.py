7e7fa12ce2b83199040786dd3e55313b79e36579
zhuyixuan <1094951135@qq.com>
2022-07-27 13:18:14 +0100

    update splitway

--- final_decision.py
+++ final_decision.py
@@ -17,20 +17,30 @@ from itertools import product
 
 N_REPETITION=[5]
 LENGTH=[10]#2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
-USE_HIS=[False]
+USE_HIS=[True]
 ADAP_POOL=[False]
 USE_HYBRID=[False]
-TOTAL=[21629+21629]#5972,21629
-NUM_EPOCHS=[80]#20,40,60,80
+TOTAL=[2986]#2986,21629
+NUM_EPOCHS=[60]#20,40,60,80
 USE_SESSION_OR_PATIENTS=['patients','sessions',None]#'patients','sessions',None
-LR=[0.005]#0.001,0.002,0.005,0.01
-filename = './training_detail.csv'
-BATCH_SIZE=[256]#8,16,32,64,128,256,512,1024
-for (n_repetition,length,use_his,adap_pool,use_hybrid,total,use_session_or_patients,batch_size,num_epochs,lr) in product(N_REPETITION,\
-    LENGTH,USE_HIS,ADAP_POOL,USE_HYBRID,TOTAL,USE_SESSION_OR_PATIENTS,BATCH_SIZE,NUM_EPOCHS,LR):
+LR=[0.01]#0.001,0.002,0.005,0.01  tuab:(0.01,64)  tueg(0.005,256)
+FIX_TESTSET=[False,True]#True,False
+BATCH_SIZE=[64]#8,16,32,64,128,256,512,1024
+BLOCK=[0,1,2,3,4,5,6,7]#0,1,2,3,4,5,6,7
+for (n_repetition,length,use_his,adap_pool,use_hybrid,total,use_session_or_patients,batch_size,num_epochs,lr,fix_testset,block) in product(N_REPETITION,\
+    LENGTH,USE_HIS,ADAP_POOL,USE_HYBRID,TOTAL,USE_SESSION_OR_PATIENTS,BATCH_SIZE,NUM_EPOCHS,LR,FIX_TESTSET,BLOCK):
     print('batch_size',batch_size)
     print('num_epochs',num_epochs)
     print('lr',lr)
+    tuab=True
+    tueg=False
+    deep4=False
+    tcn_1=True
+    if tuab:
+        filename = './training_detail_tuab.csv'
+
+    if tueg:
+        filename = './training_detail_tueg.csv'
     # n_repetition=5
     # length=7
     # use_his=True
@@ -52,58 +62,67 @@ for (n_repetition,length,use_his,adap_pool,use_hybrid,total,use_session_or_patie
     with open(filename, newline='') as csvfile:
         results = csv.reader(csvfile, delimiter=',')
         for i, row in enumerate(results):
-            if i >=0 and i<16:
-                pd_labels+=row
-            elif i>=16 and i<32:
-                pd_data+=row
-            elif i==32:
-                pd_valid_lens+=row
-                data1_len=len(pd_labels)
-                print(len(pd_labels))
-            elif i==33:
-                patients+=row
-            elif i==34:
-                sessions+=row
-            # if i >= 35 and i < 51:
-            #     pd_labels += row
-            # elif i >= 51 and i < 67:
-            #     pd_data += row
-            # elif i == 67:
-            #     pd_valid_lens += row
-            #     data1_len = len(pd_labels)
-            #     print(len(pd_labels))
-            # elif i == 68:
-            #     patients += row
-            # elif i == 69:
-            #     sessions += row
-            if i >= 35 and i < 51:
-                pd_labels += row
-            elif i >= 51 and i < 67:
-                pd_data += row
-            elif i == 67:
-                pd_valid_lens+=[int(i1)+data1_len for i1 in row]
-            elif i == 68:
-                patients += row
-            elif i == 69:
-                sessions += row
-            # elif i >=46 and i<50:
-            #     pd_labels+=row
-            # elif i>=50 and i<54:
-            #     pd_data+=row
-            # elif i==54:
-            #     pd_valid_lens+=[int(i1)+data1_len for i1 in row]
-            # elif i==55:
-            #     patients+=row
-            # elif i==56:
-            #     sessions+=row
+            if tueg:
+                if deep4:
+                    if i >=0 and i<16:
+                        pd_labels+=row
+                    elif i>=16 and i<32:
+                        pd_data+=row
+                    elif i==32:
+                        pd_valid_lens+=row
+                        data1_len=len(pd_labels)
+                        print(len(pd_labels))
+                    elif i==33:
+                        patients+=row
+                    elif i==34:
+                        sessions+=row
+                    if tcn_1:
+                        if i >= 35 and i < 51:
+                            pd_labels += row
+                        elif i >= 51 and i < 67:
+                            pd_data += row
+                        elif i == 67:
+                            pd_valid_lens+=[int(i1)+data1_len for i1 in row]
+                        elif i == 68:
+                            patients += row
+                        elif i == 69:
+                            sessions += row
+                if tcn_1:
+                    if i >= 35 and i < 51:
+                        pd_labels += row
+                    elif i >= 51 and i < 67:
+                        pd_data += row
+                    elif i == 67:
+                        pd_valid_lens += row
+                        data1_len = len(pd_labels)
+                        print(len(pd_labels))
+                    elif i == 68:
+                        patients += row
+                    elif i == 69:
+                        sessions += row
+            if tuab:
+                if i >= 35+block*11 and i < 39+block*11:
+                    pd_labels += row
+                elif i >= 39+block*11 and i < 43+block*11:
+                    pd_data += row
+                elif i == 43+block*11:
+                    pd_valid_lens += row
+                elif i == 44+block*11:
+                    patients += row
+                elif i == 45+block*11:
+                    sessions += row
+
+
     pd_valid_lens.append(len(pd_labels))
+
+
     for i in range(len(pd_valid_lens)):
         pd_valid_lens[i]=int(pd_valid_lens[i])
     for i in range(len(pd_data)):
         pd_data[i] = float(pd_data[i])
-    print('patients',len(patients))
+
+    print('recordings',len(patients))
     print('unique_patients',len(set(patients)))
-    print('sessions',len(sessions))
     print('unique_sessions',len(set(sessions)))
     true_num=0
     for i in range(len(pd_labels)):
@@ -160,7 +179,7 @@ for (n_repetition,length,use_his,adap_pool,use_hybrid,total,use_session_or_patie
     sessions_patients=[]
     for i,patient in enumerate(patients):
         sessions_patients.append(str(patient)+str(sessions[i]))
-
+    print('unique_sessions',len(set(sessions_patients)))
     # print(pd_valid_lens[-10:])
     if use_session_or_patients=='patients':
         creat_his_by_criterion(patients)
@@ -189,6 +208,10 @@ for (n_repetition,length,use_his,adap_pool,use_hybrid,total,use_session_or_patie
     # print(labels)
     # print(valid_lens)
     # print(data)
+    # labels=labels[:-4000]
+    # valid_lens=valid_lens[:-4000]
+    # data=data[:-4000]
+
     print(len(labels))
     print(len(valid_lens))
     print(len(data))
@@ -212,7 +235,8 @@ for (n_repetition,length,use_his,adap_pool,use_hybrid,total,use_session_or_patie
         idx_train, idx_test = train_test_split(np.arange(len(valid_lens)),
                                                      random_state=n_time,
                                                      train_size=0.8,
-                                                     shuffle=True)
+                                                     shuffle=not fix_testset)
+        # print(idx_train)
         idx_train, idx_valid = train_test_split(idx_train,
                                                      random_state=n_time,
                                                      train_size=0.75,
@@ -220,7 +244,7 @@ for (n_repetition,length,use_his,adap_pool,use_hybrid,total,use_session_or_patie
         test_set = torch.utils.data.Subset(dataset,idx_test)
         valid_set=torch.utils.data.Subset(dataset,idx_valid)
         train_set = torch.utils.data.Subset(dataset, idx_train)
-        #将数据集导入DataLoader，进行shuffle以及选取batch_size
+
         data_loader = DataLoader(train_set,batch_size=batch_size,shuffle=True,num_workers=0)
         data_loader_valid=DataLoader(valid_set,batch_size=batch_size,shuffle=True,num_workers=0)
 
@@ -352,19 +376,25 @@ for (n_repetition,length,use_his,adap_pool,use_hybrid,total,use_session_or_patie
         all_argmax=0
         right_mean=0
         all_mean=0
-        data_loader = DataLoader(test_set,batch_size=1,shuffle=False,num_workers=0)
+        data_loader = DataLoader(test_set,batch_size=16,shuffle=False,num_workers=0)
         for batch in data_loader:
             X, Y, valid_len = [x.to(device) for x in batch]
-            compare_res=torch.sum((X > 0.5)==Y,dim=-1)
+            compare_res=[]
+            X1=X>0.5
+            for i,j,h in zip(X1,Y,valid_len):
+                mid=i==j
+                mid1=mid[:h]
+                compare_res.append(sum((i==j)[:h]))
+            # compare_res=torch.sum((X > 0.5)==Y,dim=-1)
             right_ori+=sum(compare_res)
-            all_ori += X.numel()
-            for i in compare_res:
-                if i>valid_len/2:
+            all_ori += sum(valid_len)
+            for i,j in zip(compare_res,valid_len):
+                if i>j/2:
                     right_argmax+=1
                 all_argmax+=1
             mean_res=torch.mean(X,dim=-1)
-            for i,j in zip(mean_res,Y):
-                if (i>0.5)==j:
+            for i,j,h in zip(mean_res,Y,valid_len):
+                if (i>(0.5*h/20))==j:
                     right_mean+=1
                 all_mean+=1
 
@@ -392,8 +422,14 @@ for (n_repetition,length,use_his,adap_pool,use_hybrid,total,use_session_or_patie
     print('ave_mean_acc',ave_mean_acc)
     with open('./decision_result.csv','a') as f:
         writer=csv.writer(f, delimiter=',',lineterminator='\n',)
-        writer.writerow(['use_hybrid','n_repetition','length','use_his','adap_pool','ave_test_acc','ave_ori_acc','ave_argmax_acc','ave_mean_acc','use_session','total','epochs','time','batch_size','num_epochs','lr'])
-        writer.writerow([use_hybrid,n_repetition,length,use_his,adap_pool,ave_test_acc,ave_ori_acc,ave_argmax_acc,ave_mean_acc,use_session_or_patients,total,num_epochs,time.strftime('%Y-%m-%d_%H:%M:%S',time.localtime(time.time())),batch_size,num_epochs,lr])
+        writer.writerow(['use_hybrid','n_repetition','length','use_his','adap_pool','ave_test_acc',\
+                         'ave_ori_acc','ave_argmax_acc','ave_mean_acc','use_session','total','epochs',\
+                         'time','batch_size','num_epochs','lr','fix_testset','deep4','tcn_1','tuab','tueg',\
+                         'block'])
+        writer.writerow([use_hybrid,n_repetition,length,use_his,adap_pool,ave_test_acc,ave_ori_acc,\
+                         ave_argmax_acc,ave_mean_acc,use_session_or_patients,total,num_epochs,\
+                         time.strftime('%Y-%m-%d_%H:%M:%S',time.localtime(time.time())),batch_size,\
+                         num_epochs,lr,fix_testset,deep4,tcn_1,tuab,tueg,block])
 
 
 
